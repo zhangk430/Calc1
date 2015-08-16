e541cc2ce9f8fe9f1ad27f8440ffefa3ebc4fe59
zhangk430 <zhangk430@gmail.com>
2015-08-16 00:05:41 -0700

    Add buttons and slots.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,3 +12,43 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+    double a = ui->lineEdit->text().toDouble();
+    double b = ui->lineEdit_2->text().toDouble();
+    double result = a + b;
+    ui->lineEdit_3->setText(QString::number(result));
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    double a = ui->lineEdit->text().toDouble();
+    double b = ui->lineEdit_2->text().toDouble();
+    double result = a - b;
+    ui->lineEdit_3->setText(QString::number(result));
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    double a = ui->lineEdit->text().toDouble();
+    double b = ui->lineEdit_2->text().toDouble();
+    double result = a * b;
+    ui->lineEdit_3->setText(QString::number(result));
+}
+
+void MainWindow::on_pushButton_4_clicked()
+{
+    double a = ui->lineEdit->text().toDouble();
+    double b = ui->lineEdit_2->text().toDouble();
+    double result = a / b;
+    ui->lineEdit_3->setText(QString::number(result));
+}
+
+
+void MainWindow::on_pushButton_5_clicked()
+{
+    ui->lineEdit->setText(QString::number(0));
+    ui->lineEdit_2->setText(QString::number(0));
+    ui->lineEdit_3->setText(QString::number(0));
+}
